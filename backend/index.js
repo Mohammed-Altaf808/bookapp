f4c9a5f82261b73c129f60dabc026737d000dfbd
Mohammed Altaf <khanmohammedaltaf.7860@gmail.com>
2025-05-16 20:19:58 +0530

    moongose change

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,9 @@ dotenv.config()
 
 const methodOverride =require('method-override')
 
-mongoose.connect(process.env.MONGO_URL)
+mongoose.connect( process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,})
 .then(() => {
 console.log("MONGO CONNECTION OPEN!!!")
 })
